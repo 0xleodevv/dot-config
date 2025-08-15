1e0f155151e561831507826f0743547574b92241
Leo <leonard.paturel@openzeppelin.com>
2025-08-15 12:06:47 +0200

    reuse supermaven

--- nvim/lua/leo/plugins/code/supermaven.lua
+++ nvim/lua/leo/plugins/code/supermaven.lua
@@ -1,29 +1,8 @@
--- return {
--- 	"supermaven-inc/supermaven-nvim",
--- 	event = "InsertEnter",
--- 	opts = {},
--- 	keys = {
--- 		{ "<leader>st", "<cmd>SupermavenToggle<cr>", desc = "Toggle SuperMaven" },
--- 	},
--- }
 return {
-	"zbirenbaum/copilot.lua",
-	cmd = "Copilot",
-	build = ":Copilot auth",
-	event = "BufReadPost",
-	opts = {
-		suggestion = {
-			keymap = {
-				accept = "<Tab>",
-			},
-			suggestion = {
-				auto_trigger = true,
-			},
-		},
-		panel = { enabled = false },
-		filetypes = {
-			markdown = false,
-			help = true,
-		},
+	"supermaven-inc/supermaven-nvim",
+	event = "InsertEnter",
+	opts = {},
+	keys = {
+		{ "<leader>st", "<cmd>SupermavenToggle<cr>", desc = "Toggle SuperMaven" },
 	},
 }

4bd11e2e3f209850567ee298511df8a27fc802d5
Leo <leonard.paturel@openzeppelin.com>
2025-11-03 21:42:31 +0900

    ref: better colour scheme

--- nvim/lua/leo/plugins/ui/colourscheme.lua
+++ nvim/lua/leo/plugins/ui/colourscheme.lua
@@ -23,7 +23,10 @@ return {
 				terminal = true, -- Set to `false` to let terminal manage its own colors.
 				overrides = {
 					LineNr = { fg = "#636A72" },
+					NonText = { fg = "#636A72" },
 					["@variable.member"] = { fg = "#D2A6FF" },
+					["@lsp.type.struct.rust"] = { fg = "#04C5FF" },
+					["@lsp.mod.constant.rust"] = { fg = "#FFAA33" },
 				},
 			})
 
